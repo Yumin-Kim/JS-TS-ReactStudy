faa4f0ce367414e4afe1081cd1a4b3b710b00ed7
Yumin-Kim <dbals0@naver.com>
2020-05-12 21:23:42 +0900

    2020 05 12 Learned  webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,72 @@
-const path = require('path');
-const webpack = require("webpack");
-const nodeExtenals = require("webpack-node-externals");
-const dev = process.env.NODE_ENV !== "production";
+// const path = require('path');
+// const webpack = require("webpack");
+// const nodeExtenals = require("webpack-node-externals");
+// const dev = process.env.NODE_ENV !== "production";
 
-const backendConfig = {
-  mode: dev ? 'development' : "production",
-  target:"node",
-  entry: {
-    server: './src/server/index.tsx',
-  },
-  resolve: {
-    extensions: ['.jsx', '.js', '.tsx', '.ts'],
-  },
-  module: {
-    rules: [{
-      test: /\.(tsx||ts)?$/,
-      loader: 'awesome-typescript-loader',
-      options:{
-        useBabel:true,
-        babelCore:"@babel/core",
-        babelOptions :{
-          babelrc:true
-        }
-      }
-    }],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-  },
-  plugins:[
-    new webpack.DefinePlugin({
-      __isServer__:"true",
-    })
-  ],
-  externals:[nodeExtenals()]
-}
+// const backendConfig = {
+//   mode: dev ? 'development' : "production",
+//   target:"node",
+//   entry: {
+//     server: './src/server/index.tsx',
+//   },
+//   resolve: {
+//     extensions: ['.jsx', '.js', '.tsx', '.ts'],
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.(tsx||ts)?$/,
+//       loader: 'awesome-typescript-loader',
+//       options:{
+//         useBabel:true,
+//         babelCore:"@babel/core",
+//         babelOptions :{
+//           babelrc:true
+//         }
+//       }
+//     }],
+//   },
+//   output: {
+//     filename: '[name].js',
+//     path: path.join(__dirname, 'dist'),
+//   },
+//   plugins:[
+//     new webpack.DefinePlugin({
+//       __isServer__:"true",
+//     })
+//   ],
+//   externals:[nodeExtenals()]
+// }
 
-const frontConfig ={
-  mode: dev ? 'development' : "production",
-  entry: {
-    build: './src/client/index.tsx',
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts',".js",".jsx"],
-  },
-  module: {
-    rules: [{
-      test: /\.(tsx||ts)?$/,
-      loader: 'awesome-typescript-loader',
-      options:{
-        useBabel:true,
-        babelCore:"@babel/core",
-        babelOptions :{
-          babelrc:true
-        }
-      }
-    }],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-  },
-  plugins:[
-    new webpack.DefinePlugin({
-      __isServer__:"false"
-    })
-  ]
-}
+// const frontConfig ={
+//   mode: dev ? 'development' : "production",
+//   entry: {
+//     build: './src/client/index.tsx',
+//   },
+//   resolve: {
+//     extensions: ['.tsx', '.ts',".js",".jsx"],
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.(tsx||ts)?$/,
+//       loader: 'awesome-typescript-loader',
+//       options:{
+//         useBabel:true,
+//         babelCore:"@babel/core",
+//         babelOptions :{
+//           babelrc:true
+//         }
+//       }
+//     }],
+//   },
+//   output: {
+//     filename: '[name].js',
+//     path: path.join(__dirname, 'dist'),
+//   },
+//   plugins:[
+//     new webpack.DefinePlugin({
+//       __isServer__:"false"
+//     })
+//   ]
+// }
 
-module.exports = [frontConfig];
+// module.exports = [frontConfig];
---
+++ webpack/webpack.config_.js
@@ -0,0 +1,104 @@
+//Resolve environment and import
+const environment = process.env.NODE_ENV;
+const autoprefixer = require("autoprefixer");
+const webpack = require("webpack");
+const MiniCssExtractplugin = require("mini-css-extract-plugin");
+const htmlPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+//Entry
+const entry = {
+    "main": ["./source/main.site.scss","./source/main.site.ts"]
+}
+
+
+//why need to exclude node_modules in webpack
+//First
+//Nearly all packages are written in js not in ts, it is not going to harm if we include node_modules
+//Excluding node_modules at the transfile stage increate performance which could otherwise get a hit
+//Second
+//If we are referencing a pakeage that is written ts, we definitely want ts code to de complied to js code,then we have to include node_modules to make sure everything works. dont we?
+//Yes. and then is the Key here. Excluding node_modules at the transpling stage doesn`t prevent webpack from its content at the bundling stage
+const _module = {
+    rules: [
+        {
+            test: /\.(tsx||ts)?$/,
+            loader: 'awesome-typescript-loader',
+            options: {
+                useBabel: true,
+                babelCore: "@babel/core",
+                babelOptions: {
+                    babelrc: true
+                }
+            }
+        }, {
+            test: /\.component.scss$/,
+            // exclude: ["node_modules", "0-bourbon", "1-neat", "2-base"],\ㅊ
+            use: [
+                "raw-loader",//this module can import   to the transforming txt file
+                "sass-loader"
+            ]
+        }, {
+            test: /\.site.scss$/,
+            // exclude: ["node_modules", "0-bourbon", "1-neat", "2-base"],
+            use: [
+                {
+                    loader: MiniCssExtractplugin.loader,
+                    options: {
+                        hmr: environment === "development" ? true : false,
+                    }
+                }, "css-loader", "sass-loader"
+            ]
+        }
+    ]
+}
+
+const optimization = {
+    splitChunks: {
+        cacheGroups: {
+            commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "common",
+                    chunks: "all"
+               }
+            }
+        }
+    }
+
+
+
+const output = {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname + "../wwroot/js/"),
+    pathinfo: true,
+}
+
+if (environment === "production") {
+    output.filename = "[name].bundle.min.js";
+    output.pathinfo = false;
+} else if (environment === "development") {
+    output.publicPath = "/js/";
+}
+
+const plugins = [
+    new MiniCssExtractplugin({
+        filename: '[name].css',
+        chunkname: '[id].css',
+        ignoreOrder: false,
+    }),
+    new htmlPlugin(),
+]
+
+const resolve = {
+    extensions: [".ts", ".js", ".jsx", ".tsx", ".css", "scss"]
+}
+
+module.exports = {
+    mode: environment === "development" ? "development" : "production",
+    entry,
+    output,
+    resolve,
+    plugins,
+    optimization,
+    module: _module,
+}
