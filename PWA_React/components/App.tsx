fe12455be87517ba2c571c4309a5ee70e0f401c8
Yumin-Kim <dbals0@naver.com>
2020-06-02 23:59:19 +0900

    2020 06 02 PWA_Tutorial

---
+++ PWA_React/components/App.tsx
@@ -0,0 +1,15 @@
+import * as  React from 'react'
+
+const App = () =>{
+    return(
+        <>
+        <h1>
+            Hello
+        </h1>
+        <img src="./image/image1.jpg" alt="asd"></img>
+        <img src="./image/image2.jpg" alt="asd"></img>
+        <img src="./image/image3.jpg" alt="asd"></img>
+        </>
+    )
+}
+export default App;
