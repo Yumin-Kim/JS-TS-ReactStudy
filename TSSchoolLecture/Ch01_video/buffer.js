f4056fbb5f91dea530cf37d1981bbe8f6d388102
Yumin-Kim <dbals0@naver.com>
2020-08-31 21:38:05 +0900

    start Node Tutorial

---
+++ TSSchoolLecture/Ch01_video/buffer.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const readStream = fs.createReadStream("./read.txt");
+
+const buffer = Buffer.from("출력 하겠습니다");
+console.log(buffer);
+console.log(buffer.length);
+
+const arrayBuffer = [Buffer.from("Hello"), Buffer.from("Hello World"), Buffer.from("Hello World Node")];
+
+const BufferArrary = Buffer.concat(arrayBuffer);
+
+console.log(BufferArrary.toString());
+
+const data  = [];
+readStream.on("data", (chunk) => {
+    data.push(chunk);
+    console.log("Chunk Data : " , chunk, chunk.length);
+});
+
+readStream.on("end",()=>{
+    console.log("end : " , Buffer.concat(data).length , Buffer.concat(data).toString() );
+})
