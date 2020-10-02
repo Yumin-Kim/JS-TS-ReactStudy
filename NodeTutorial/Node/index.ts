981e1fe7657656688d0c5ac148ca82b044c9303b
Yumin-Kim <dbals0@naver.com>
2020-10-02 14:26:35 +0900

    Typescript(Node) and javscript(prototype)

--- NodeTutorial/Node/index.ts
+++ NodeTutorial/Node/index.ts
@@ -1,3 +1,22 @@
+import * as chapter from "../Module/ch0930";
+
+const ch0930 : chapter.AddParam = {
+    First_num :1,
+    End_num : 10,
+    callback : (First_num , End_num)=>{
+        let ResultNum =0;
+        for(let i = First_num ; i <=End_num ; i++ )
+        {
+            ResultNum +=i;
+        }
+        return ResultNum;
+    }
+};
+
+console.log(chapter.chapter);
+console.log(chapter.chapter_1);
+console.log(chapter.addCallBackFunc(ch0930));
+
 interface I_Student{
     num : number;
     name : String | number;
@@ -12,3 +31,19 @@ const StudentObject : I_Student ={
 
 StudentObject.add = function(a,b){return a+b;}
 console.log(`학번 과 이름 ${StudentObject.num} ${StudentObject.name} \n산술 100 + 100 : ${StudentObject.add(100,100)}`)
+
+class TypescriptClass {
+    public at;
+    constructor() {
+        this.at = 100;
+    }
+}
+console.log(new TypescriptClass());
+console.log(Object.prototype.constructor());
+let Num :number = 0;
+const ResultNum  = Array(10).fill("").forEach((val:string,index) : number=>{
+    Num += index+1;
+    return 10;
+})
+
+console.log(Num);
