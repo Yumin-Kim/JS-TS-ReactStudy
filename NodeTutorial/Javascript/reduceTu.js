087a7b26b7105ecd00b75995157de478da5d7657
Yumin-Kim <dbals0@naver.com>
2020-10-16 09:43:22 +0900

    Used ReduceMethod Array

---
+++ NodeTutorial/Javascript/reduceTu.js
@@ -0,0 +1,23 @@
+const student = ["홍길동", "이서연", "안은영"];
+const eng = [40, 90, 20];
+const math = [50, 70, 30];
+const kor = [100, 50, 60];
+const result = Array(3).fill().reduce((prev, cur, idx) => {
+    prev[idx] = eng[idx] + math[idx] + kor[idx];
+    return prev;
+}, [])
+const average = Array(3).fill().reduce((prev, cur, idx) => {
+    prev[idx] = Math.ceil(result[idx] / result.length);
+    return prev;
+}, [])
+const category = ["student", "eng", "math", "kor", "result", "average"]
+const categoryData = [student, eng, math, kor, result, average];
+
+console.log(categoryData)
+for (let i = 0; i < student.length; i++) {
+    const PersonInfo = category.reduce((prev, cur, curidx, arry) => {
+        prev[`${cur}`] = categoryData[curidx][i]
+        return prev
+    }, {})
+    console.log(PersonInfo);
+}
