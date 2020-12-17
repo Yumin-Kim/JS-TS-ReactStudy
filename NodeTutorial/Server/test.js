aea83f53df4530789fe8e63e65c90a7653121af4
Yumin-Kim <dbals0@naver.com>
2020-12-17 23:13:53 +0900

    End Point  Node.js Final Test

--- NodeTutorial/Server/test.js
+++ NodeTutorial/Server/test.js
@@ -6,229 +6,83 @@ const express = require('express');
 const static = require('serve-static');
 const bodyParser = require('body-parser');
 //  external Module - config
-const dotenv = require("dotenv");
 const mysql = require('mysql');
-const { addUser } = require('./Config/Mysql');
-const { threadId } = require('worker_threads');
 //config Variable
 const app = express();
-const PORT = 4000;
+const PORT = 4040;
 
 //Config Middleware 
-dotenv.config()
 app.use(static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
-    password: process.env.DB_PASSWORD !== null ? process.env.DB_PASSWORD : "",
-    database: 'test_DB',
+    password: "",
+    database: '201610309_db',
     debug: false
 })
 
-
-const connectionAndQueryFuc = () =>{
-    pool.getConnection((error,conn)=>{
-        if (error) throw error;
-        conn.query(str,(err,result)=>{
-            if (err) throw err;
-                res.end()
-                conn.release()           
-        })
-    })
-}
-
-///////////////////////////이미지 라우팅하기
-const hemster = express.Router();
-hemster.route('/hemimage').post((req, res) => {
-    const {number,picture,category,number1,picture1} = req.body;
-    console.log('/hemimage 호출됨');
+const printData = express.Router();
+printData.route('/printData').post((req, res) => {
+    const { id, address, age, password } = req.body;
     res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
-    res.write('<h2>이미지 보여줍니다</h2>');
-    if(Number(number) === 0) {
-        res.write(`<img src="${picture}.jpg"><br><h1>그냥하나준다 퉤</h1></img>`);
-        res.end();
-    }else{
-        for(let i = 0 ; i < number ; i++){
-            res.write(`<img src="${picture}.jpg"><h1>사진이요</h1></img>`);
-        }
-        res.end();
-    }
+    res.write('<h1>201610309_김유민</h1>');
+    res.write(`<p>이름 : ${id}</p>`)
+    res.write(`<p>학년 : ${password}</p>`)
+    res.write(`<p>나이 : ${age}</p>`)
+    res.write(`<p>주소 : ${address}</p>`)
+    res.end();
 });
-app.use(hemster);
+app.use(printData);
 
-//////////////////////////데이터 읽기
+const covid19 = express.Router();
+covid19.route('/covid19').post((req, res) => {
+    console.log('/covid19 호출됨');
+    res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+    res.write('<h2>김유민 코로나 19</h2>');
+    res.write(`<img src="covid19.jpg"><br><h1> 김유민_코로나 19</h1></img>`);
+    res.end();
+});
+
+app.use(covid19);
 const mysqlRead = express.Router();
 mysqlRead.route('/mysqlRead').post((req, res) => {
-    console.log('/mysqlRea 호출됨');
-    pool.getConnection((err, conn) => {
-
-        const str = "select * from test_td";
-        conn.query(str, (err, result) => {
+    console.log('/mysqlRead 호출됨');
+    pool.getConnection((error, conn) => {
+        const str = `select * from  a201610309_table `;
+        conn.query(str, function (err, result) {
             res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-            res.write("사용자<br>");
-
-            for (const i = 0; i < result.length; i++)
-                res.write(result[i].id + "<br>"); //서버에 등록된거 전부 찍는 부분
-
+            res.write("<h1>정보</h1>")
+            for(let i = 0 ; i < result.length ; i++){
+                res.write(` <br> ${result[i].name} : ${result[i].grade} ${result[i].age} ${result[i].address}</br>`)
+            }
             res.end();
             conn.release();
-
         });
 
     });
-});
+})
 
-app.use(mysqlRead);
-
-//////////////////////////데이터 쓰기
-
-const mysqlInsert = express.Router();
-mysqlInsert.route('/mysqlInsert').post(function (req, res) {
-    console.log('/mysqlInsert 호출됨');
-    const id = req.body.id;
-    const pw = req.body.pw;
-    // pool.getConnection(function (err, conn) {
-    //     const str = "insert into test_td(id,pass)values('" + id + "','" + pw + "')";
-    //     conn.query(str, function (err, result) {
-    //         console.log(err,result)
-    //         res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-    //         res.write("사용자[" + id + "]추가되었습니다.");
-    //         res.end();
-    //         conn.release;
-    //     });
-
-    // });
-
-    pool.getConnection(function (err, conn) {
-        const str = `delete from test_td where id = ${id} `;
+const MysqlInsert = express.Router();
+MysqlInsert.route("/MysqlInsert").post((req, res) => {
+    const { name, grade, address, age } = req.body;
+    pool.getConnection((error, conn) => {
+        const str = `insert into a201610309_table (name,grade,address,age)values("${name}","${grade}","${address}","${age}")`;
         conn.query(str, function (err, result) {
             console.log(err, result)
             res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-            res.write("사용자[" + id + "]추가되었습니다.");
-            res.end();
-            conn.release;
-        });
-
-    });
-
-});
-app.use(mysqlInsert);
-/////////////////////////////////////////////////서버 돌리는 것
-/////////////////////////////////////////////////Tutorial
-//Create db
-let U_tableName = "test";
-const createTable = express.Router();
-createTable.route("/addDBtable").post((req, res) => {
-    const { tablename } = req.body;
-    U_tableName = tablename;
-    console.log("입력값 : ", tablename);
-    pool.getConnection((error, conn) => {
-        const queryStr = `CREATE table ${tablename}(id char(10),password char(15) ,address char(10),age int(45)) `;
-        conn.query(queryStr, (error, result) => {
-            try {
-                console.log("addDB")
-                res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-                res.write(`<p>${queryStr}</p>`)
-                res.write(`사용자 ${tablename}추가되었습니다.`);
-                res.end();
-                conn.release();
-            }
-            catch (err) {
-                console.log("err")
-                console.log(error)
-            }
-        })
-    })
-});
-app.use(createTable);
-///////////////////////////////////////
-//insertColum
-const insertColum = express.Router();
-insertColum.route("/insertColum").post((req, res) => {
-    const { id, password, address, age } = req.body;
-    pool.getConnection((error, conn) => {
-        const str = `insert into ${U_tableName}(id,password,address,age)values("${id}","${password}","${address}","${age}")`;
-        conn.query(str, function (err, result) {
-            console.log(err, result)
-            res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-            res.write(`tablename ${U_tableName} \n사용자 ${id} \n ${password} \n ${address} \n ${age}추가되었습니다.`);
+            res.write(`\n사용자 [${name} / ${grade} / ${address} / ${age}]추가되었습니다.`);
             res.end();
             conn.release();
         });
     })
 });
-app.use(insertColum);
-///////////////////////////////////////
-//selectAlltable
-const selectAlltable = express.Router();
-selectAlltable.route("/selectAlltable").post((req,res)=>{
-    const {id} = req.body;
-    console.log(id === undefined);
-    const selectQuert = {
-        all : `select * from ${U_tableName}`,
-        one : `select * from ${U_tableName} where id = "${id}"`,
-    }
-    pool.getConnection((error,conn)=>{
-        if (error) throw error;
-        conn.query(id !== undefined ? selectQuert.one : selectQuert.all ,(error,result)=>{
-            res.writeHead(200, { "Content-Type": "Text/html;charset=utf8" });
-            console.log(result)
-            for (let i = 0; i < result.length; i++){
-                res.write("\n사용자"+[i+1]+"<br>");
-                res.write(result[i].id + " "); //서버에 등록된거 전부 찍는 부분
-                res.write(result[i].password + " "); //서버에 등록된거 전부 찍는 부분
-                res.write(result[i].address + " "); //서버에 등록된거 전부 찍는 부분
-                res.write(result[i].age + "<br>"); //서버에 등록된거 전부 찍는 부분
-            }
-            res.end();
-            conn.release();
-        })
-    })
-})
-app.use(selectAlltable);
-////////////////////////////////////////
-//updateColum
-const updateColum = express.Router();
-updateColum.route("/updateColum").post((req,res)=>{
-    const {changeid,id} = req.body;
-    pool.getConnection((error,conn)=>{
-        if (error) throw error;
-        const query = `Update ${U_tableName} set id = "${changeid}" where id = "${id}"`
-        conn.query(query,(err,result)=>{
-            if(err) throw err;
-            console.log(result.affectedRows + "Update record");
-            res.send("<p>Update Record<p>");
-            res.end();
-            conn.release();
-        })
-    })
-})
-app.use(updateColum);
-//////////////////////////////////////////////////
-///deletColum
-const deletColum = express.Router();
-deletColum.route("/deletColum").get((req,res)=>{
-    const {id} = req.query;
-    console.log(req.body)
-    const query = `Delete FROM ${U_tableName} WHERE id = "${id}"` 
-    pool.getConnection((error,conn)=>{
-        if (error) throw error;
-        conn.query(query,(err,result)=>{
-            if (err) throw err;
-            console.log(result);
-            console.log(id);
-            res.end();
-            conn.release();
-        })
-    })
-})
-app.use(deletColum);
+app.use(MysqlInsert);
 
-
+app.use(mysqlRead)
 http.createServer(app).listen(PORT, function () {
-    console.log('서버가시작되었습니다 포트 :' + PORT);
+    console.log('20160309 김유민 서버시작 :' + PORT);
 })
 
 
